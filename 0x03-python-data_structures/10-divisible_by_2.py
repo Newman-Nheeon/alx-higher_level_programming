b74e83b4fdc54eec284128af9e7441c4ee8cbeac
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-07-04 19:35:18 +0100

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    newlist = my_list
+    for i in range(newlist):
+        if 1 % 2 == 0:
+            return True
+        else:
+            return False
